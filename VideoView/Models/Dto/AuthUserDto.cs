15f98adec09d72eb509a2e7520177fa54475c755
Appci11 <61198490+Appci11@users.noreply.github.com>
2023-02-25 18:24:39 +0200

    uploading kol veikia

---
+++ VideoView/Models/Dto/AuthUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoView.Models.Dto
+{
+    public class AuthUserDto
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool returnSecureToken { get; set; } = true;
+    }
+}
